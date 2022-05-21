f6422a29b5ad38d70ec7ae45da5fdabd4f1ca940
Wilian <wilian.hw@hotmail.com>
2022-05-21 18:45:54 -0300

    Fundamentos básicos

---
+++ aula4_variaveis/Variavel1.java
@@ -0,0 +1,12 @@
+public class Variavel1 {
+    public static void main(String args[]) {
+        int quantidade; //Declarando variável
+        quantidade = 10; //Atribuindo o valor 10 a variável 10;
+
+        System.out.println(quantidade);
+
+        quantidade = 15; //Atribuindo o valor 15 a variável 15.
+
+        System.out.println(quantidade);
+    }
+}
---
+++ aula5_operadores_aritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,12 @@
+public class OperadoresAritmeticos {
+    public static void main(String args[]) {
+        int valor1 = 10;
+        int valor2 = 5;
+
+        System.out.println("Somatório: " + (valor1 + valor2));
+        System.out.println("Subtração: " + (valor1 - valor2));
+        System.out.println("Multiplicação: " + (valor1 * valor2));
+        System.out.println("Divisão: " + (valor1 / valor2));
+        System.out.println("Modulo: " + (valor1 % valor2));
+    }
+}
