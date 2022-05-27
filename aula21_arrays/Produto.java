90231f2bd58f9da954ec926975296c79c1f9a3c3
Wilian <wilian.hw@hotmail.com>
2022-05-26 23:42:32 -0300

    Wrappers, boxing e arrays

---
+++ aula21_arrays/Produto.java
@@ -0,0 +1,8 @@
+public class Produto {
+    String descricao;
+    Integer quantidade;
+
+    void descrever() {
+        System.out.println("Nome: " + descricao + " - Quantidade: " + quantidade);
+    }
+}
